dccb295ade2e3e0689f48703f7eadf36cf5c54c0
bonejam <charlotte.cooney@connect.qut.edu.au>
2023-10-26 19:11:08 +1000

    edited lvl 1

---
+++ game-level/Assets/Scripts/ConveyorBelt.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+  //  [SerializeField]
+ //   private float speed, conveyourSpeed;
+  //  [SerializeField]
+  //  private Vector3 direction;
+   // [SerializeField]
+   // private List<GameObject> onBelt;
+
+   // private Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  material = GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+      //  GetComponent<MeshRenderer>().
+    }
+}
